ccfc7d650e8c1368f62de2c9f173687bb9beeda7
james-clarke-audio <59263848+james-clarke-audio@users.noreply.github.com>
2024-05-07 11:55:57 +0100

    Add Documents to repo
    Add base documents to repository

---
+++ include/classtest.h
@@ -0,0 +1,27 @@
+/******************************************************************************
+ * Project: stm32 - E4                                                        *
+ * -----                                                                      *
+ * Copyright 2024 - James Clarke                                              *
+ * -----                                                                      *
+ * Licence:                                                                   *
+ *     Use of this source code is governed by an MIT-style                    *
+ *     license that can be found in the LICENSE file or at                    *
+ *     https://opensource.org/licenses/MIT.                                   *
+ ******************************************************************************/
+#ifndef CLASSTEST_H
+#define CLASSTEST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Code goes here */
+
+/******************************************************************************
+ ******************************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CLASSTEST_H
